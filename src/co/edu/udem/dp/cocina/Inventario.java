81de236d5b4ceb21767528ba7496d61a91fed11e
John Alexis Munera <alexismunera98@gmail.com>
2019-09-12 23:14:50 -0500

    First version Inventario

--- src/co/edu/udem/dp/cocina/Inventario.java
+++ src/co/edu/udem/dp/cocina/Inventario.java
@@ -1,4 +1,22 @@
 package co.edu.udem.dp.cocina;
 
+import javafx.util.Pair;
+
+import java.util.List;
+import java.util.Map;
+
 public class Inventario {
+    private static Inventario inventario = null;
+    Map<IngredienteABS, Integer> cantidadPorIngrediente;
+
+    public static Inventario getInstance() {
+        if (inventario == null) {
+            inventario = new Inventario();
+        }
+        return inventario;
+    }
+
+    public void addInventario(IngredienteABS ingrediente, Integer cantidad) {
+        cantidadPorIngrediente.replace(ingrediente, cantidadPorIngrediente.get(ingrediente) + cantidad);
+    }
 }
